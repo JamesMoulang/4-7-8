aed9f10ef78df1959973a9ee47f880c8e49d6aaa
James Moulang <jamesmoulang123@gmail.com>
2017-02-16 01:04:31 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+"use strict";
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var config = require('./webpack.config.js');
+var uid = require('uid');
+var _ = require('underscore');
+var app = express();
+var compiler = webpack(config);
+var Player = require('./player');
+var Room = require('./room');
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  noInfo: true,
+  publicPath: config.output.publicPath
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.use(express.static(path.join(__dirname, 'app', 'assets')));
+
+app.get('*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'app', 'index.html'));
+});
+
+var server = require('http').createServer(app);
+
+server.listen(process.env.PORT || 3000, function() {
+	console.log("listening on port 3000");
+});
